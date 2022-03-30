0d3642a24940f987719c4d4aa1aee49d447a6489
gogoup <15705486620@163.com>
2022-03-30 16:30:28 +0800

    3.30代码初次更新

---
+++ application/extra/addons.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'autoload' => false,
+    'hooks' => [],
+    'route' => [
+        '/example$' => 'example/index/index',
+        '/example/d/[:name]' => 'example/demo/index',
+        '/example/d1/[:name]' => 'example/demo/demo1',
+        '/example/d2/[:name]' => 'example/demo/demo2',
+    ],
+    'priority' => [],
+    'domain' => '',
+];
